396b527c5aa2f470788d84c8dffcae6b40015dd3
AndrewsWxCenter <39846531+AndrewsWxCenter@users.noreply.github.com>
2019-03-08 21:53:07 -0500

    Create IfModule mod_expires.c

---
+++ IfModule mod_expires.c
@@ -0,0 +1,10 @@
+<IfModule mod_expires.c>
+	ExpiresActive On
+	ExpiresDefault A0
+	ExpiresByType image/gif A691200
+	ExpiresByType image/png A691200
+	ExpiresByType image/jpeg A691200
+	ExpiresByType text/css A691200
+	ExpiresByType text/javascript A691200
+	ExpiresByType application/javascript A691200
+</IfModule>
